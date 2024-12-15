1d9860048876b95437f92e22238a96d221202990
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-15 22:05:46 +0500

    Add user entity

--- src/auth/domain/user/entity.py
+++ src/auth/domain/user/entity.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 
-from app.domain.base.entity import Entity
-from app.domain.user.enums import UserRoleEnum
-from app.domain.user.value_objects import UserId, Username, UserPasswordHash
+from auth.domain.base.entity import Entity
+from auth.domain.user.enums import UserRoleEnum
+from auth.domain.user.value_objects import UserId, Username, UserPasswordHash
 
 
 @dataclass(eq=False, kw_only=True)
-class User(Entity[UserId]): ...
+class User(Entity[UserId]):
+    username: Username
+    password_hash: UserPasswordHash
+    roles: set[UserRoleEnum]
